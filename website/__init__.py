8109c6ff7312745a45b6f9270af1910c6035689d
valyo <582646+valyo@users.noreply.github.com>
2023-05-31 18:34:10 +0200

    use debug mode dureing dev

--- website/__init__.py
+++ website/__init__.py
@@ -4,6 +4,7 @@ from flask import Flask
 def create_app():
     app = Flask(__name__)
     app.config["SECRET_KEY"] = "fjsldjhfdljslfhlajhfjasdf"
+    app.config["FLASK_DEBUG"] = 1
 
     from .views import views
 
